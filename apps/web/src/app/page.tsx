ebc19ebcb25e186efc886deab379667d037296db
helloa1109 <helloa1109@naver.com>
2025-10-21 14:15:33 +0900

    feat:add second section

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import { Header } from '@/components/Header';
 import { HeroSection } from '@/components/HeroSection';
+import { SecondSection } from '@/components/SecondSection';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white overflow-x-hidden">
       <Header />
       <HeroSection />
+      <SecondSection/>
     </main>
   );
 }
